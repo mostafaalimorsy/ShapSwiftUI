af97f2525da3f6e5e8dba9670132cc3d5d010b75
Mostafa Morsy <mostafa.morsy@intalio.com>
2025-02-18 16:52:47 +0200

    readme file

---
+++ ShapSwiftUI/Shap.swift
@@ -0,0 +1,20 @@
+//
+//  Shap.swift
+//  ShapSwiftUI
+//
+//  Created by mostafa Morsy on 18/02/2025.
+//
+
+import SwiftUI
+import Foundation
+
+struct Shap: Shape {
+    func path(in rect: CGRect) -> Path {
+        <#code#>
+    }
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
