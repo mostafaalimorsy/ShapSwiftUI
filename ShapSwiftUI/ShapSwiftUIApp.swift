8fadb2718873c06e6dbf543363c769f1f0c7b74d
Mostafa Morsy <mostafa.morsy@intalio.com>
2025-02-18 16:46:22 +0200

    Initial Commit

---
+++ ShapSwiftUI/ShapSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapSwiftUIApp.swift
+//  ShapSwiftUI
+//
+//  Created by mostafa Morsy on 18/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShapSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShapSwiftUITests/ShapSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  ShapSwiftUITests.swift
+//  ShapSwiftUITests
+//
+//  Created by mostafa Morsy on 18/02/2025.
+//
+
+import Testing
+@testable import ShapSwiftUI
+
+struct ShapSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
